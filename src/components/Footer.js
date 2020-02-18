7daf0fdca7139a3b62d90fcc5aaa1e4e282b23e4
aakanksha1 <aakanksha720@gmail.com>
2020-02-18 08:03:30 -0600

    added footer, fixed image sizing

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles/Footer.scss";
+
+export default function Banner() {
+
+  return (
+    <footer className="footer">
+      <p className="footer-text">&copy;&nbsp;Rinse & Repeat. All rights reserved.</p>
+    </footer>
+  );
+}
