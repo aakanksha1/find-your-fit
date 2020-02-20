6d335a6c6932b4bbc8664802a00f16a8b77cb429
Andrew Chung <drew.c3071@gmail.com>
2020-02-20 14:12:47 -0600

    update logo and gitignore to include .firebase

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -9,7 +9,7 @@ const Banner = () => {
     <Navbar bg="dark" variant="dark" className="navigation-bar" sticky="top">
       <Navbar.Brand className="home">
         <Link to="/">
-          <h2 className="title">rinse. repeat.</h2>
+          <h2 className="title">find your fit.</h2>
         </Link>
       </Navbar.Brand>
       <Nav className="ml-auto"></Nav>
