65c9adb19aea186b9c5280235cf8c95668b9b940
aakanksha1 <aakanksha720@gmail.com>
2020-03-02 22:23:03 -0600

    progress

--- src/components/ProductPage.js
+++ src/components/ProductPage.js
@@ -17,12 +17,8 @@ const ProductPage = () => {
   const { id } = useParams();
   const [product, setProduct] = useState(null);
   const [size, setSize] = useState(null);
-<<<<<<< HEAD
   const [color, setColor] = useState(null);
   const isEnabled = size !== null && color !== null;
-=======
-  const [pcolor, setColor] = useState(null);
->>>>>>> e8999d93d4a7f1d4e9fe626fcf53fc44efe57ffb
 
   useEffect(() => {
     db.collection("items")
@@ -58,58 +54,59 @@ const ProductPage = () => {
             <p>Color:</p>
             <div className="color-button-group">
               {product.colors.map((color, i) => (
+
                 <OverlayTrigger
                   placement="top"
                   overlay={props => <Tooltip {...props}>{color.value}</Tooltip>}
                 >
                   <button
-                    id="color-button"
                     key={i}
-                    className={pcolor === color.value ? "yellow" : null}
+                    className={color === i ? "red" : null}
                     onClick={() => {
-                      pcolor === i ? setColor("") : setColor(color.value);
-                      console.log(pcolor);
+                      color === i
+                        ? setColor("")
+                        : setColor(color.value);
                     }}
-                    style={{ backgroundColor: color.hex }}
-                  ></button>
+                    style={{ backgroundColor: color.hex }}>
+                    {i}
+
+                  </button>
                 </OverlayTrigger>
               ))}
             </div>
 
             <p>Size:</p>
-            <div className="color-button-group">
+            <div>
               {["XS", "S", "M", "L", "XL"].map(s => (
-                <button
-                  className={size === s ? "black" : null}
-                  id="size-button"
-                  onClick={() => {
-                    size === s ? setSize("") : setSize(s);
-                  }}
-                  key={s}
-                >
-                  {s}
-                </button>
+                <div>
+                  <a
+                    className={size === s ? "red" : null}
+                    onClick={() => {
+                      size === s
+                        ? setSize("")
+                        : setSize(s);
+                    }}
+                    key={s}
+                  >
+                    {s}
+                  </a>
+                </div>
               ))}
             </div>
-
             <ButtonGroup className="purchase-button-group">
-              <Link
-                to={{
-                  pathname: `/checkout`,
-                  state: {
-                    checkoutProduct: product,
-                    checkoutProductSize: size,
-                    checkoutProductColor: pcolor,
-                    purchaseType: "trial"
-                  }
-                }}
-              >
-                {/* `/checkout/${id}`} className="product-link"> */}
+              <Link to={{
+                pathname: `/checkout`,
+                state: {
+                  checkoutProduct: product,
+                  checkoutProductSize: size,
+                  checkoutProductColor: color,
+                  purchaseType: 'trial'
+                }
+              }}>
                 <Button variant="outline-dark" disabled={!isEnabled}>
                   Try ${(0.25 * product.price).toFixed(2)}{" "}
                 </Button>
               </Link>
-<<<<<<< HEAD
               <Link to={{
                 pathname: `/checkout`,
                 state: {
@@ -120,23 +117,8 @@ const ProductPage = () => {
                 }
               }}>
                  <Button variant="outline-dark" disabled={!isEnabled}>
-=======
-              <Link
-                to={{
-                  pathname: `/checkout`,
-                  state: {
-                    checkoutProduct: product,
-                    checkoutProductSize: size,
-                    checkoutProductColor: pcolor,
-                    purchaseType: "buy"
-                  }
-                }}
-              >
-                <Button variant="outline-dark">
->>>>>>> e8999d93d4a7f1d4e9fe626fcf53fc44efe57ffb
                   Buy ${product.price.toFixed(2)}
-                </Button>
-              </Link>
+                </Button></Link>
             </ButtonGroup>
             <Card.Text>
               <hr className="line"></hr>
