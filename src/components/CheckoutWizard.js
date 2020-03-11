824fd6e1e343d31076c5454de934e550a1aeaf9f
Batuhan Ergor <batuhanergor2021@u.northwestern.edu>
2020-03-10 20:21:51 -0500

    checkout workflow complete

--- src/components/CheckoutWizard.js
+++ src/components/CheckoutWizard.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Row, Col, Container, Card } from "react-bootstrap";
+import { Row, Col, Container, Card, Form, Button } from "react-bootstrap";
 import { useParams, Link, useLocation } from "react-router-dom";
 import ReactDOM from "react-dom";
 import CheckoutPage from "./CheckoutPage";
@@ -54,15 +54,39 @@ class MasterForm extends React.Component {
    */
   previousButton() {
     let currentStep = this.state.currentStep;
-    if (currentStep !== 1) {
+    if (1 < currentStep && currentStep < 3) {
       return (
-        <button
-          className="btn btn-secondary"
-          type="button"
-          onClick={this._prev}
+        <Col
+          xs={1}
+          sm={{ span: 1, offset: 0 }}
+          md={{ span: 1, offset: 0 }}
+          lg={{ span: 1, offset: 0 }}
         >
-          Previous
-        </button>
+          <Button variant="secondary" type="button" onClick={this._prev}>
+            Previous
+          </Button>
+        </Col>
+      );
+    }
+    return null;
+  }
+  takeHomeButton() {
+    let currentStep = this.state.currentStep;
+    if (currentStep === 3) {
+      return (
+        <div>
+          <br></br>
+          <Col
+            xs={7}
+            sm={{ span: 7, offset: 5 }}
+            md={{ span: 7, offset: 5 }}
+            lg={{ span: 7, offset: 5 }}
+          >
+            <Button variant="dark" type="button" size="lg" href="/">
+              Back to Home Page
+            </Button>
+          </Col>
+        </div>
       );
     }
     return null;
@@ -72,13 +96,16 @@ class MasterForm extends React.Component {
     let currentStep = this.state.currentStep;
     if (currentStep < 3) {
       return (
-        <button
-          className="btn btn-primary float-right"
-          type="button"
-          onClick={this._next}
+        <Col
+          xs={1}
+          sm={{ span: 1, offset: 8 }}
+          md={{ span: 1, offset: 8 }}
+          lg={{ span: 1, offset: 8 }}
         >
-          Next
-        </button>
+          <Button variant="dark" type="button" onClick={this._next}>
+            Next
+          </Button>
+        </Col>
       );
     }
     return null;
@@ -87,7 +114,7 @@ class MasterForm extends React.Component {
   render() {
     return (
       <React.Fragment>
-        <p> {this.state.currentStep} </p>
+        {/* <p> {this.state.currentStep} </p>*/}
 
         <form onSubmit={this.handleSubmit}>
           {/* 
@@ -110,6 +137,7 @@ class MasterForm extends React.Component {
           />
           {this.previousButton()}
           {this.nextButton()}
+          {this.takeHomeButton()}
         </form>
       </React.Fragment>
     );
@@ -174,7 +202,7 @@ function Step1(props) {
             name="lname"
             type="text"
             placeholder="Phone Number"
-            value="+3129998877"
+            value="+1(312)999-8877"
           />
         </Col>
       </Row>
@@ -329,56 +357,128 @@ function Step2(props) {
     return null;
   }
   return (
-    <div className="form-group">
-      <h2> Billing Information</h2>
-      <h4> Credit Card Information</h4>
-      <input
-        className="form-control"
-        id="lname"
-        name="lname"
-        type="text"
-        placeholder="Name on Card"
-        value="Batuhan Ergor"
-      />
-      <input
-        className="form-control"
-        id="lname"
-        name="lname"
-        type="text"
-        placeholder="Card Number"
-        value="111222333444"
-      />
-      <input
-        className="form-control"
-        id="lname"
-        name="lname"
-        type="text"
-        placeholder="Expiration Date"
-        value="12/21"
-      />
-      <input
-        className="form-control"
-        id="lname"
-        name="lname"
-        type="text"
-        placeholder="CVV"
-        value="999"
-      />
-      <h4>Billing Address</h4>
-      <label for="shipping">Same as Shipping Address</label>
-      <input
-        className="form-control"
-        id="shipping"
-        name="lname"
-        type="checkbox"
-      ></input>
-      <label for="billing">Enter New Address </label>
-      <input
-        className="form-control"
-        id="shipping"
-        name="lname"
-        type="checkbox"
-      ></input>
+    <div>
+      <Row className="header">
+        <Col
+          xs={12}
+          sm={{ span: 6, offset: 1 }}
+          md={{ span: 6, offset: 1 }}
+          lg={{ span: 6, offset: 1 }}
+        >
+          <h2>PAYMENT</h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={12}
+          sm={{ span: 6, offset: 1 }}
+          md={{ span: 6, offset: 1 }}
+          lg={{ span: 6, offset: 1 }}
+        >
+          <h5> CREDIT CARD INFORMATION</h5>
+        </Col>
+      </Row>
+      <Row className="input_row">
+        <Col
+          xs={8}
+          sm={{ span: 8, offset: 1 }}
+          md={{ span: 8, offset: 1 }}
+          lg={{ span: 8, offset: 1 }}
+        >
+          <input
+            className="form-control"
+            id="lname"
+            name="lname"
+            type="text"
+            placeholder="City"
+            value="Batuhan Ergor"
+          />
+        </Col>
+      </Row>
+      <Row className="input_row">
+        <Col
+          xs={8}
+          sm={{ span: 8, offset: 1 }}
+          md={{ span: 8, offset: 1 }}
+          lg={{ span: 8, offset: 1 }}
+        >
+          <input
+            className="form-control"
+            id="lname"
+            name="lname"
+            type="text"
+            placeholder="City"
+            value="1111222233334444"
+          />
+        </Col>
+      </Row>
+      <Row className="input_row">
+        <Col
+          xs={4}
+          sm={{ span: 4, offset: 1 }}
+          md={{ span: 4, offset: 1 }}
+          lg={{ span: 4, offset: 1 }}
+        >
+          <input
+            className="form-control"
+            id="fname"
+            name="fname"
+            type="text"
+            placeholder="First Name"
+            value="12/21"
+          />
+        </Col>
+        <Col
+          xs={6}
+          sm={{ span: 4, offset: 0 }}
+          md={{ span: 4, offset: 0 }}
+          lg={{ span: 4, offset: 0 }}
+        >
+          <input
+            className="form-control"
+            id="fname"
+            name="fname"
+            type="text"
+            placeholder="Last Name"
+            value="999"
+          />
+        </Col>
+      </Row>
+      <br></br>
+      <Row>
+        <Col
+          xs={12}
+          sm={{ span: 6, offset: 1 }}
+          md={{ span: 6, offset: 1 }}
+          lg={{ span: 6, offset: 1 }}
+        >
+          <h5> BILLING ADDRESS</h5>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          xs={12}
+          sm={{ span: 6, offset: 1 }}
+          md={{ span: 6, offset: 1 }}
+          lg={{ span: 6, offset: 1 }}
+        >
+          <Form>
+            <Form.Check
+              type="radio"
+              label="Same as Shipping Address"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios1"
+            />
+            <Form.Check
+              type="radio"
+              label="Enter New Address"
+              name="formHorizontalRadios"
+              id="formHorizontalRadios2"
+            />
+          </Form>
+        </Col>
+      </Row>
+      <br></br>
     </div>
   );
 }
